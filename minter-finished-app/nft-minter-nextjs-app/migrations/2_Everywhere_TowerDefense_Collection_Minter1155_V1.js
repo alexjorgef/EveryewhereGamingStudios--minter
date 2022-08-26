e1db86f1a6fcf7fd29ade94fa35930560725c8b3
Brandon Kozak <brandon@w3evolutions.com>
2022-08-26 09:16:01 -0400

    add migrations and deployers for ganache

---
+++ minter-finished-app/nft-minter-nextjs-app/migrations/2_Everywhere_TowerDefense_Collection_Minter1155_V1.js
@@ -0,0 +1,6 @@
+var Demo = artifacts.require("_Everywhere_TowerDefense_Collection_Minter1155_V1");
+
+module.exports = function(deployer) {
+    // Demo is the contract's name
+    deployer.deploy(Demo);
+};
